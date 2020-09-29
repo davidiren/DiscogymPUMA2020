7df6a0047393dbe028cf6ac5581bf28d2f24ae7c
David <id17din@cs.umu.se>
2020-09-29 13:04:08 +0200

    Första commit autogen kod endast

---
+++ DiscogymPUMA2020/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DiscogymPUMA2020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
