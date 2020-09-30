ae198e2f5cb466b0b465badd895616c90623b667
id17dlm <id17dlm@cs.umu.se>
2020-09-30 18:07:36 +0200

    New class added

---
+++ DiscogymPUMA2020/Models/Class/Mood.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscogymPUMA2020.Models.Class
+{
+    public class Mood
+    {
+        public Mood()
+        {  }
+        public int Id { get; set;}
+        public string Name { get; set;}
+
+    }
+}
---
+++ DiscogymPUMA2020/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscogymPUMA2020.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+    }
+}
