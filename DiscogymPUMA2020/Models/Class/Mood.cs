473a5e8d6bd9ec35426fa94affa4a3fdaf8611fc
id17dlm <id17dlm@cs.umu.se>
2020-09-30 19:12:54 +0200

    -Added interfaces and fixed context file

--- DiscogymPUMA2020/Models/Class/Mood.cs
+++ DiscogymPUMA2020/Models/Class/Mood.cs
@@ -8,9 +8,13 @@ namespace DiscogymPUMA2020.Models.Class
     public class Mood
     {
         public Mood()
-        {  }
+        {
+            Logs = new HashSet<Log>();
+        }
         public int Id { get; set;}
         public string Name { get; set;}
+
+        public virtual ICollection<Log> Logs { get; set; }
 
     }
 }
--- DiscogymPUMA2020/Models/Context.cs
+++ DiscogymPUMA2020/Models/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DiscogymPUMA2020.Models.Class;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,17 @@ namespace DiscogymPUMA2020.Models
         {
 
         }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Exercise> Exercise { get; set; }
+        public DbSet<ExerciseGoal> ExerciseGoal { get; set; }
+        public DbSet<FavoriteExercise> FavoriteExercise { get; set; }
+        public DbSet<Log> Log { get; set; }
+        public DbSet<Mood> Mood { get; set; }
+        public DbSet<Plan> Plan { get; set; }
+        public DbSet<Workout> Workout { get; set; }
+        public DbSet<WorkoutExercise> WorkoutExercise { get; set; }
+
     }
 }
