1e86de832858125d8fe1c0e280b0adaf2e587da8
id17dlm <id17dlm@cs.umu.se>
2020-10-02 12:34:11 +0200

    --Fixed repo classes

--- DiscogymPUMA2020/Models/Repository/ExerciseRepo.cs
+++ DiscogymPUMA2020/Models/Repository/ExerciseRepo.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using DiscogymPUMA2020.Models.Class;
+using DiscogymPUMA2020.Models.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DiscogymPUMA2020.Models.Repository
 {
-    public class ExerciseRepo
+    public class ExerciseRepo : IExerciseRepo
     {
+        protected readonly Context context;
+        public ExerciseRepo(Context _context)
+        {
+            context = _context;
+        }
+        public IEnumerable<Exercise> GetExercises => throw new NotImplementedException();
+
+        public Exercise GetExercise(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Exercise> GetExercisesByCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Exercise> GetExercisesByLevel(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
