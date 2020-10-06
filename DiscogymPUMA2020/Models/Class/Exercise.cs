9574bfc11b027e2e9c73f253c67eb7be7a6b2858
idalil <idalil@hotmail.se>
2020-10-06 10:25:23 +0200

    Exercise done

--- DiscogymPUMA2020/Models/Class/Exercise.cs
+++ DiscogymPUMA2020/Models/Class/Exercise.cs
@@ -7,5 +7,21 @@ namespace DiscogymPUMA2020.Models.Class
 {
     public class Exercise
     {
+        public Exercise()
+
+            {
+
+            }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public int ExerciseLevelId { get; set; }
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+        public string Video { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ExerciseLevel ExerciseLevel { get; set; }
+
     }
 }
