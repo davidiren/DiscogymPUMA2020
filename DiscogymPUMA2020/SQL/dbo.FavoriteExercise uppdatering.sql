3e629e7b58a4715378904922ecb11ec56c59d3d9
David <id17din@cs.umu.se>
2020-10-06 13:29:58 +0200

    favoriter har fått ID

---
+++ DiscogymPUMA2020/SQL/dbo.FavoriteExercise uppdatering.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[FavoriteExercise] (
+	[Id]		INT NOT NULL,
+    [UserId]    INT NOT NULL,
+    [WorkoutId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id]),
+    FOREIGN KEY ([WorkoutId]) REFERENCES [dbo].[Workout] ([Id])
+);
+
