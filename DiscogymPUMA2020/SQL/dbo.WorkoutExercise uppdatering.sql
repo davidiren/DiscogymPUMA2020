d8733011d25d40eb9cb5293e9f38a7e6b6853204
David <id17din@cs.umu.se>
2020-10-06 13:40:41 +0200

    workoutexercise fick ett id

---
+++ DiscogymPUMA2020/SQL/dbo.WorkoutExercise uppdatering.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[WorkoutExercise] (
+	[Id]		 INT IDENTITY(1,1),
+    [WorkoutId]  INT NOT NULL,
+    [ExerciseId] INT NOT NULL,
+	PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([WorkoutId]) REFERENCES [dbo].[Workout] ([Id]),
+    FOREIGN KEY ([ExerciseId]) REFERENCES [dbo].[Exercise] ([Id])
+);
+
